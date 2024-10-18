9ebba1522e864e75c814d0a8ff3471e3756f3ca0
Roman Romanovsky <romanromnovskyy@gmail.com>
2024-10-18 21:42:14 +0200

    ss

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import re
 
-
-def check_passwor(password):
-    check_pas = re.compile(r'[a-zA-Z0-9!@#$%^&*()_+={}\[\]|:;"\'<>,.?/-=`~]+$')
-    if check_pas.match (password):
-        print('valid')
-    else:
-        print('not valid')
-
-
-with open('password.txt')as p:
-    for i in p:
-        print(check_passwor(i.strip()))
+with open("new:txt", 'w')as new:
+    new.write('thi is my file txt')
