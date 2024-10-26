657bb70b414bc074fa6ec0b083d01e1a9b7f6475
Roman Romanovsky <romanromnovskyy@gmail.com>
2024-10-26 18:10:53 +0200

    ss

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import re
+from email.message import EmailMessage
+import smtplib
+from string import Template
+from pathlib import Path
 
-with open("new.txt", 'w')as new:
-    new.write('thi is my file txt')
+my_email = EmailMessage()
+
+html_templlate = Template(Path("templates/index.html").read_text())
+html_cont = html_templlate.substitute({'name': 'Irene', 'date': 'tomorow'})
+
+my_email['to'] = 'irebades@icloud.com'
+my_email['subject'] = "lets go out"
+my_email.set_content(html_cont, 'html')
+
+with smtplib.SMTP("smtp.gmail.com", 587)as smtp_server:
+    smtp_server.ehlo()
+    smtp_server.starttls()
+    smtp_server.login('romanovskyyroman@gmail.com', 'korv ogvl ztoy qxss')
+    smtp_server.send_message(my_email)
+    print('succes')
